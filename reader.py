220da34171b72362061c205dd3a81248f6209863
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2015-07-02 16:12:30 +0800

    Add reader for pptx files.

--- reader.py
+++ reader.py
@@ -6,26 +6,44 @@ import zipfile
 import sys
 from lxml import etree
 
-def readOfficeXml(filename, xmlname):
-    try:
-        f = zipfile.ZipFile(filename, "r")
-        xml = f.read(xmlname)
-        f.close()
+def extractTextFromXml(xml):
+    if xml:
         xml = etree.fromstring(xml)
         notags = etree.tostring(xml, encoding='utf8', method='text')
         return notags.decode("utf8", errors="ignore")
-    except:
-        print ("Error reading:", filename)
     return ""
 
-def readDocxFile(filename):
-    return readOfficeXml(filename, "word/document.xml")
+
+def readOfficeXml(filename):
+    content = ""
+    try:
+        f = zipfile.ZipFile(filename, "r")
+        xml = None
+        if filename.endswith(".docx"):
+            xml = f.read("word/document.xml")
+            content = extractTextFromXml(xml)
+        elif filename.endswith(".pptx"):
+            files = f.namelist()
+            slides = []
+            i = 1
+            while True:
+                slide_name = "ppt/slides/slide%d.xml" % (i)
+                if slide_name not in files:
+                    break
+                xml = f.read(slide_name)
+                slides.append(extractTextFromXml(xml))
+                i += 1
+            content = ''.join(slides)
+        f.close()
+    except:
+        print ("Error reading:", filename, sys.exc_info())
+    return content
 
 
 def main():
     if len(sys.argv) < 2:
         return 1
-    print(readDocxFile(sys.argv[1]))
+    print(readOfficeXml(sys.argv[1]))
 
 if __name__ == "__main__":
     main()
